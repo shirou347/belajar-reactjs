6f50609592a794f73ea1edf18fbe82a829684a60
shirou347 <shirou347@gmail.com>
2023-12-13 18:28:33 +0700

    bikin navbar pake bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavbarComponent from "./components/NavbarComponent";
+
+const App = () => {
+  return (
+    <div>
+      <NavbarComponent />
+      <h1>ini adalah h1 test doang</h1>
+      <button>Saya adalah tombol</button>
+    </div>
+  );
+};
+
+export default App;
